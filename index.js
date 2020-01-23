df592e576f8d689a7dc037e73587e4643283ee5c
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-23 03:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const returnFirstTwoDrivers = function(drivers) {
 const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(drivers.length - 2, drivers.length);
 };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(timesFare) {
+  return function (fare) {
+    return timesFare * fare;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, whosDriving) {
+  return whosDriving(drivers);
+};
